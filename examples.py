d99d82f2632c7a4a666b4d7a9e6f08c59b0b12ad
cmpmk <chamlink@hotmail.com>
2018-10-26 09:16:19 -0700

    Removed todo list, merged test script with example script

--- examples.py
+++ examples.py
@@ -1,5 +1,4 @@
 from pylab import *
-
 from numid import *
 
 # Example using Euler's Method for ODEs:
@@ -8,7 +7,7 @@ xn, yn = eul(f, 1, 1, 1.5)
 
 # Plot Numerical (Euler's) vs Analytical Solution:
 # Analytical Solution:
-x = array([1.0, 1.1, 1.2, 1.3, 1.4, 1.5])
+x = linspace(1, 1.5, 6)
 y = 0.36787*exp(x**2)
 
 plot(x, y, 'r', label='analytical')
@@ -16,8 +15,23 @@ plot(xn, yn, 'b', label='numerical')
 legend()
 show()
 
-
 # Example for double integration via midpoint method:
 #def f(x,y):
 #    return 2*x+y
 #midpoint_dbl(f, 0, 2, 2, 3, 5, 5)
+
+# Test the calculation times of trapezoidal and trapvec methods
+import time
+from numid import trapezoidal, trapvec
+
+f = lambda x: sin(exp(x) -((cos(exp(5)))))
+
+start = time.time()
+print(trapezoidal(f, 1, 100, 1000000))
+end = time.time()
+print('trapezoidal finished in ', (end-start))
+
+start = time.time()
+print(trapvec(f, 1, 100, 1000000))
+end = time.time()
+print('trapvec finished in ', (end-start))
